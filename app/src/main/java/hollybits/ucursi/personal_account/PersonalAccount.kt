dea1d13ec3e154890b0e28efbf7354bb58e316a9
Victor Yezhov <yezhov.victor@gmail.com>
2019-05-29 10:56:28 +0300

    fetching logginned user info

--- app/src/main/java/hollybits/ucursi/personal_account/PersonalAccount.kt
+++ app/src/main/java/hollybits/ucursi/personal_account/PersonalAccount.kt
@@ -2,13 +2,16 @@ package hollybits.ucursi.personal_account
 
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.Observer
+import com.google.android.gms.auth.api.Auth
 
 import hollybits.ucursi.R
+import hollybits.ucursi.main.AuthDataViewModel
 import kotlinx.android.synthetic.main.fragment_test_data_shower.*
 import kotlinx.android.synthetic.main.personal_account_fragment.*
 import kotlinx.android.synthetic.main.personal_account_fragment.view.*
@@ -17,9 +20,11 @@ class PersonalAccount : Fragment() {
 
     companion object {
         fun newInstance() = PersonalAccount()
+        private const val TAG = "PersonalAccount"
     }
 
     private lateinit var userDataViewModel: AccountViewModel
+    private lateinit var authViewModel:AuthDataViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,19 +35,24 @@ class PersonalAccount : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        userDataViewModel = ViewModelProviders.of(this).get(AccountViewModel::class.java)
-        userDataViewModel.loadAccountData("zavaliy@ucu.edu.ua", "VUNVQXBwVmVyc2lvbjAuN3NhdWx0")
+        userDataViewModel = ViewModelProviders.of(activity!!).get(AccountViewModel::class.java)
+        authViewModel = ViewModelProviders.of(activity!!).get(AuthDataViewModel::class.java)
+        userDataViewModel.loadAccountData(authViewModel.getAuthenticatedUser()?.email!!, "VUNVQXBwVmVyc2lvbjAuN3NhdWx0")
         setupDataListeners()
 
     }
 
     private fun setupDataListeners(){
 
+        userDataViewModel.isDataLoaded.observe(this, Observer {
+            loading_layout.visibility = View.GONE
+        })
         userDataViewModel.userName.observe(this, Observer { userName->
             userNameUA.text = userName
         })
 
         userDataViewModel.userEmail.observe(this, Observer { email ->
+            Log.i(TAG, "setting email: $email")
             userEmail.text = email
         })
 
--- app/src/main/java/hollybits/ucursi/personal_account/data/User.kt
+++ app/src/main/java/hollybits/ucursi/personal_account/data/User.kt
@@ -7,11 +7,11 @@ import com.google.gson.annotations.SerializedName
 
 data class User (
 
-	@SerializedName("login") val login : String,
-	@SerializedName("name") val name : String,
-	@SerializedName("surname") val surname : String,
-	@SerializedName("email") val email : String,
-	@SerializedName("dateBirth") val dateBirth : String,
-	@SerializedName("phone") val phone : String,
-	@SerializedName("displayName") val displayName : String
+	@SerializedName("login") val login : String?,
+	@SerializedName("name") val name : String?,
+	@SerializedName("surname") val surname : String?,
+	@SerializedName("email") val email : String?,
+	@SerializedName("dateBirth") val dateBirth : String?,
+	@SerializedName("phone") val phone : String?,
+	@SerializedName("displayName") val displayName : String?
 )
